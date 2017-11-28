009e5b7a02756c45bff792004056edcfc7b8d6f8
chaorong <chaorong@maichong.it>
2017-11-28 18:22:42 +0800

    bug

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-exports.default = function (props) {
-  return layout(props || {});
-};
+exports.default = parseReact;
 
 var _server = require('react-dom/server');
 
@@ -14,33 +11,14 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _fs = require('fs');
-
-var _fs2 = _interopRequireDefault(_fs);
-
 var _Index = require('./renderer/Index');
 
 var _Index2 = _interopRequireDefault(_Index);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function layout(data) {
-  let style = _fs2.default.readFileSync(__dirname + '/css/style.min.css');
+function parseReact(data) {
   // let content = renderToString(<Index value={data} />);
-  let content = (0, _server.renderToStaticMarkup)(_react2.default.createElement(_Index2.default, { value: data }));
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <link href="http://cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-      <style type="text/css">${style}</style>
-    </head>
-    <body>
-      <div id="app">
-          ${content}
-      </div>
-    </body>
-    </html>
-  `;
+  // let content = renderToStaticMarkup(<Index value={data} />);
+  return (0, _server.renderToStaticMarkup)(_react2.default.createElement(_Index2.default, { value: data }));
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,9 @@
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToStaticMarkup, renderToString } from 'react-dom/server';
 import React from 'react';
-import fs from 'fs';
 import Index from './renderer/Index';
 
-function layout(data) {
-  let style = fs.readFileSync(__dirname + '/css/style.min.css');
+export default function parseReact(data) {
   // let content = renderToString(<Index value={data} />);
-  let content = renderToStaticMarkup(<Index value={data} />);
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <link href="http://cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-      <style type="text/css">${style}</style>
-    </head>
-    <body>
-      <div id="app">
-          ${content}
-      </div>
-    </body>
-    </html>
-  `;
-}
-
-export default function (props) {
-  return layout(props || {});
+  // let content = renderToStaticMarkup(<Index value={data} />);
+  return renderToStaticMarkup(<Index value={data} />);
 }
